945dc2ceb51a864d52ec93e3efb72343c7bdc9e9
LourenceQ <lqfmecatronica@gmail.com>
2022-02-01 15:26:23 -0300

    Método HttpPost Login em IdentityController. Crição de Signup page

--- DotnetIdentity/Controllers/IdentityController.cs
+++ DotnetIdentity/Controllers/IdentityController.cs
@@ -1,19 +1,49 @@
 ﻿using DotnetIdentity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DotnetIdentity.Controllers
 {
     public class IdentityController : Controller
-    {        
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public async Task<IActionResult> Signup()
         {
             var model =  new SignupViewModel();
             return View(model);
         }
+
         [HttpPost]
         public async Task<IActionResult> Signup(SignupViewModel model)
         {            
+            if (ModelState.IsValid)
+            {
+                if((await _userManager.FindByEmailAsync(model.Email)) == null)
+                {
+                    var user = new IdentityUser
+                    {
+                        Email = model.Email,
+                        UserName = model.Email
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Signin");
+                    }
+
+                    ModelState.AddModelError("Signup", string.Join("", result.Errors.Select(x => x.Description)));
+                    return View(model);
+                }
+            }
             return View(model);
         }
 
